e1617ea0e328bb41d5ae0551211d8cfd973c0cbc
Maria <marysiahalamska@gmail,com>
2026-04-17 16:49:57 +0200

    localstorage

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const counter1 = document.getElementById("counter");
 const parag = document.getElementById("paragraph");
 const liliaL = document.getElementById("lilialight");
 const liliaD = document.getElementById("liliadark");
-
-let count = 0;
+const reset = document.querySelector("#resetbtn");
+let count = parseInt(localStorage.getItem("count")) || 0;
+counter1.textContent = count;
 
 button1.onclick = function () {
   count++;
   counter1.textContent = count;
+  localStorage.setItem("count", count);
 
   parag.classList.toggle("dark");
   document.body.classList.toggle("dark");
 
   button1.classList.toggle("dark");
+  reset.classList.toggle("dark");
   this.innerHTML = "you clicked me";
 
   if (document.body.classList.contains("dark")) {
@@ -26,6 +29,12 @@ button1.onclick = function () {
     liliaD.classList.replace("visible", "hidden");
     liliaL.classList.replace("hidden", "visible");
   }
+
+  reset.addEventListener("click", function () {
+    count = 0;
+    counter1.textContent = count;
+    localStorage.setItem("count", count);
+  });
 
   switch (count) {
     case 3:
